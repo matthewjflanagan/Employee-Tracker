382f11e6bb5fe11f663709a3b2a8412da369953c
Matthew Flanagan <mattflanagan40@gmail.com>
2021-11-16 19:55:21 -0500

    tables made for schema & added to seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,21 @@ CREATE DATABASE employee;
 USE employee;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
-)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,18 @@
-INSERT INTO department (id, name)
-VALUES (001, "Department A"),
-       (002, "Department B"),
-       (003, "Department C"),
-       (004, "Department D");
+INSERT INTO department (name)
+VALUES ("Department Sales"),
+       ("Department Accounting"),
+       ("Department Customer Service"),
+       ("Department Corporate");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Manager", 20000.00, 1),
+       ("Sales", 10000.00, 1),
+       ("SalesC", 15000.00, 1),
+       ("SalesD", 5000.00, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sales", "Guy", 1, null),
+       ("Sales", "NewGuy", 2, 1),
+       ("SalesC", "Veteran", 3, 1),
+       ("SalesD", "Intern", 4, 1);
+
